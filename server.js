073c06a99c5fa2bfcb1929e00fedf490d7553649
Clervens Volcy <clervens.volcy@gmail.com>
2014-12-14 17:56:35 -0500

    MOD : use generated port number or 3000

--- server.js
+++ server.js
@@ -164,7 +164,7 @@ app.get('/', function(req, res) {
 });
 
 
-server.listen(3000);
-console.log('Listening on port 3000...');
+server.listen(process.env.PORT || 3000);
+console.log('Listening on port '+(process.env.PORT || 3000)+'...');
 
 
